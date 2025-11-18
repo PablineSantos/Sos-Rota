29335815b7672fc028512a0c536b92abaff9177c
Gabriel Machado <gabrielchiles.engenharia@gmail.com>
2025-11-17 21:04:14 -0300

    first commit

---
+++ src/main/java/com/example/sos_rota_emergency_dispatch/SosRotaEmergencyDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.sos_rota_emergency_dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosRotaEmergencyDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosRotaEmergencyDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sos_rota_emergency_dispatch/SosRotaEmergencyDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sos_rota_emergency_dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosRotaEmergencyDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
