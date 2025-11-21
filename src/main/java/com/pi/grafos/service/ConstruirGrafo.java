149c8a1855fc67ed646968d75945355393d11836
PablineSantos <pablines2006@gmail.com>
2025-11-21 16:29:47 -0300

    Criação das classes Aresta, BuscarGrafo, ConstruirGrafo e LeituraGrafo(Lógica do grafo)

---
+++ src/main/java/com/pi/grafos/service/ConstruirGrafo.java
@@ -0,0 +1,32 @@
+package com.pi.grafos.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstruirGrafo {
+
+	private Map<Integer, List<Aresta>> caminho = new HashMap<>();
+
+	public void addAresta(int origem, int destino, double distancia) {
+		if (!caminho.containsKey(origem)) {
+			caminho.put(origem, new ArrayList<Aresta>());
+		}
+
+		caminho.get(origem).add(new Aresta(destino, distancia));
+		
+		if (!caminho.containsKey(destino)) {
+			caminho.put(destino, new ArrayList<Aresta>());
+		}
+		caminho.get(destino).add(new Aresta(origem, distancia));
+	}
+
+	public Map<Integer, List<Aresta>> getCaminho() {
+
+		return caminho;
+	}
+}
